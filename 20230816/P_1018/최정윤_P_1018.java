c0371ca20b88d4c695a3a97fbba11353638ff564
ehgud619 <139290855+ehgud619@users.noreply.github.com>
2023-08-16 10:39:08 +0900

    Update 최정윤_P_1018.java

--- 20230816/P_1018/최정윤_P_1018.java
+++ 20230816/P_1018/최정윤_P_1018.java
@@ -15,7 +15,7 @@ public class Problem_1018 {
             }
         }
         int min = 2500;
-        for (int i = 0; i < N - 7; i++) {// 시작점
+        for (int i = 0; i < N - 7; i++) {// 시작점 고정 알파벳 유지
             for (int j = 0; j < M - 7; j++) {
                 int count = 0;
                 for (int k = i; k < i+8; k++) {
@@ -29,21 +29,22 @@ public class Problem_1018 {
                 min = Math.min(count, min);
             }
         }
-        
-        for (int i = N-1; i >=7; i--) {// 끝점
-            for (int j = M-1; j >=7; j--) {
+        for (int i = 0; i < N - 7; i++) {// 시작점이랑 반대로 ex)B이면 W로 =>시작점 고정알파벳 바꾸는거
+            for (int j = 0; j < M - 7; j++) {
                 int count = 0;
-                for (int k = i; k > i-8; k--) {
-                    for (int l = j; l > j-8; l--) {
-                        if ((-k - l+i+j) % 2 == 0 && !arr[i][j].equals(arr[k][l]) )// 짝수는 시작점과 같아야함 다르면 고치기
+                for (int k = i; k < i+8; k++) {
+                    for (int l = j; l < j+8; l++) {
+                        if ((k + l-i-j) % 2 == 0 && arr[i][j].equals(arr[k][l]) )// 짝수는 시작점과 달라야함 같으면 고치기
                             count++;
-                        else if ((-k - l+i+j) % 2 == 1 && arr[i][j].equals(arr[k][l]))// 홀수는 시작점과 달라야함 같으면 고치기
+                        else if ((k + l-i-j) % 2 == 1 && !arr[i][j].equals(arr[k][l]))// 홀수는 시작점과 같아야함 다르면 고치기
                             count++;
                     }
                 }
                 min = Math.min(count, min);
             }
         }
+        
+
         System.out.println(min);
     }
 }

f56a7741df0a00eea298b678f8052a9327307d25
jungyounn <141210401+jungyounn@users.noreply.github.com>
2023-08-16 15:20:02 +0900

    Update 최정윤_P_12788.java

--- 20230816/P_12788/최정윤_P_12788.java
+++ 20230816/P_12788/최정윤_P_12788.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
-
+//12788번
 public class Problem_12788 {
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));

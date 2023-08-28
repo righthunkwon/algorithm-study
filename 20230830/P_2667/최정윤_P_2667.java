a83a038dc55911c2a8f6f072ab56e8dc47357cfe
jungyounn <141210401+jungyounn@users.noreply.github.com>
2023-08-28 11:03:04 +0900

    Update 최정윤_P_2667.java

--- 20230830/P_2667/최정윤_P_2667.java
+++ 20230830/P_2667/최정윤_P_2667.java
@@ -16,12 +16,12 @@ public class Problem_2667_단지번호붙이기 {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				if (arr[i][j] == '1') {
-					arr[i][j]='0';
-					list.add(danji(i, j)+1);
+					arr[i][j]='0';//1이면 0으로 우선 바꾸고 나중에 cnt+1
+					list.add(danji(i, j)+1);//리스트에 단지 수 넣기
 				}
 			}
 		}
-		Collections.sort(list);
+		Collections.sort(list);//오름차순 정렬
 		System.out.println(list.size());
 		for (int i = 0; i < list.size(); i++) {
 			System.out.println(list.get(i));
@@ -31,16 +31,17 @@ public class Problem_2667_단지번호붙이기 {
 	public static char[][] arr;
 	public static int N;
 
-	public static int danji(int R, int C) {
+	public static int danji(int R, int C) {//4방탐색하는 메소드,count를 반환
 		int[] dr = { -1, 1, 0, 0 };
 		int[] dc = { 0, 0, -1, 1 };
 		int count = 0;
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 4; i++) {//4방 탐색
 			if (R + dr[i] >= 0 && R + dr[i] < N && C + dc[i] >= 0 && C + dc[i] < N
 					&& arr[R + dr[i]][C + dc[i]] == '1') {
-				count++;
-				arr[R + dr[i]][C + dc[i]] = '0';
-				count += danji(R + dr[i], C + dc[i]);
+				//범위 안이고 1이면
+				count++;//카운트 +1
+				arr[R + dr[i]][C + dc[i]] = '0';//그 자리 0,
+				count += danji(R + dr[i], C + dc[i]);//그 자리부터 다시 탐색
 			}
 		}
 		return count;

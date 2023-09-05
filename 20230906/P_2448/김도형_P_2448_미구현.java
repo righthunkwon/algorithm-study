40c5ce22ae67ab9996ebb5e581c1b46c00264645
ehgud619 <139290855+ehgud619@users.noreply.github.com>
2023-09-06 01:23:36 +0900

    Create 김도형_P_2448_미구현.java

---
+++ 20230906/P_2448/김도형_P_2448_미구현.java
@@ -0,0 +1,40 @@
+//N은 항상 3×(2^k) 수 ...
+
+package gold;
+
+import java.util.Scanner;
+
+public class BaekJoon_Q2448_별찍기_11 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		String star1 = "  *  ";
+		String star2 = " *** ";
+		String star3 = "*****";
+
+		String blank1 = " ";
+		String blank2 = "   ";
+		String blank3 = "     ";
+		
+		
+	}//main
+	
+	
+	public static void writeStar() {
+		
+
+		
+	}
+	
+	public static void writeBlank() {
+		
+		
+		
+	}
+	
+	
+}//class

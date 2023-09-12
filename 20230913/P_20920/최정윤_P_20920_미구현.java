0d39a4abd6ad38921b03a537aa0eea19d4c7e400
jungyounn <141210401+jungyounn@users.noreply.github.com>
2023-09-12 19:21:14 +0900

    Create 최정윤_P_20920_미구현.java

---
+++ 20230913/P_20920/최정윤_P_20920_미구현.java
@@ -0,0 +1,69 @@
+
+package baek;
+
+import java.io.*;
+import java.util.*;
+//시간초과뜸....
+public class Pro_20902_영단어암기는괴로워 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		Map<String, Integer> map = new HashMap<>();
+		List<String> list = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			String word = br.readLine();
+			if (word.split("").length >= M) {
+				if (map.get(word) != null) {
+					int count = map.get(word);
+					count++;
+					map.put(word, count);
+				} else {
+					map.put(word, 1);
+				}
+			}
+		}
+		list.addAll(map.keySet());
+		Collections.sort(list, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				// TODO Auto-generated method stub
+
+				if(map.get(o1)!=map.get(o2)) {
+					return map.get(o2) - map.get(o1);
+				}else if(o1.length() != o2.length()) {
+					return o2.length() - o1.length();
+				}else {
+					return o1.compareTo(o2);
+				}
+				
+			}
+		});
+
+		StringBuilder sb = new StringBuilder();
+		for (String key : list) {
+			sb.append(key + "\n");
+		}
+		System.out.println(sb.toString());
+
+	}
+}
+//				// 빈도수가 높은 거가 앞인거 -1가 맞다
+//
+//				if (map.get(o1) > map.get(o2)) {
+//					return -1;
+//				} else if (map.get(o1) < map.get(o2)) {
+//					return 1;
+//				} else {
+//
+//					if (o1.length() > o2.length()) {
+//						return -1;
+//					} else if (o1.length() < o2.length()) {
+//						return 1;
+//					} else {
+//						return o1.compareTo(o2);// 사전순 작은게 더 앞으로
+//					}
+//				}

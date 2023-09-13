5d8bd47ee102675d443af10d72d51b8ef41d6c64
jungyounn <141210401+jungyounn@users.noreply.github.com>
2023-09-13 09:45:06 +0900

    Update 최정윤_P_2589.java

--- 20230913/P_2589/최정윤_P_2589_미구현.java
+++ 20230913/P_2589/최정윤_P_2589_미구현.java
@@ -24,7 +24,7 @@ public class Pro_2589_보물섬 {
 
 				if (map[i][j] == 'L') {
 					visited = new boolean[R][C];
-					max = Math.max(max, find(i, j, 0));
+					 find(i, j);
 				}
 			}
 		}
@@ -39,29 +39,36 @@ public class Pro_2589_보물섬 {
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 	static int max;
-	// static int su;
+	static int su;
 	static int R;
 	static int C;
 
-	public static int find(int r, int c, int su) {
-		Queue<Integer> queue = new LinkedList<Integer>();
-		visited[r][c]=true;
+	public static void find(int r, int c) {
+		Queue<int[]> queue = new LinkedList<>();
+		visited[r][c] = true;
 		for (int i = 0; i < 4; i++) {
 			if (r + dr[i] >= 0 && r + dr[i] < R && c + dc[i] >= 0 && c + dc[i] < C && map[r + dr[i]][c + dc[i]] == 'L'
 					&& visited[r + dr[i]][c + dc[i]] == false) {
 				visited[r + dr[i]][c + dc[i]] = true;
-				queue.add(i);
+
+				queue.add(new int[] { r + dr[i], c + dc[i], 1 });
 			}
 		}
-		if (queue.isEmpty())
-			return su;
-		su++;
-		while (!queue.isEmpty()) 
-		{//여기서 다시 dfs되어서 틀림 bfs로 구현해야함
-			int i = queue.poll();
-			max = Math.max(max, find(r + dr[i], c + dc[i], su));
+		while (!queue.isEmpty()) {
+			int[] j = queue.poll();
+			int nr = j[0];
+			int nc = j[1];
+			int su = j[2];
+			max = Math.max(max, su);
+			for (int i = 0; i < 4; i++) {
+				if (nr + dr[i] >= 0 && nr + dr[i] < R && nc + dc[i] >= 0 && nc + dc[i] < C
+						&& map[nr + dr[i]][nc + dc[i]] == 'L' && visited[nr + dr[i]][nc + dc[i]] == false) {
+					visited[nr + dr[i]][nc + dc[i]] = true;
+					queue.add(new int[] { nr + dr[i], nc + dc[i],su+1});//??????????????????????????????
+				}
+			}
+
 		}
-		return max;
 	}
 
 }

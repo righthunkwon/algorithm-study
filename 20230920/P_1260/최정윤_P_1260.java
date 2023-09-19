7ef173710ec62018aea46b1be0f642faa8d584d3
jungyounn <141210401+jungyounn@users.noreply.github.com>
2023-09-20 08:26:01 +0900

    Update 최정윤_P_1260.java

--- 20230920/P_1260/최정윤_P_1260.java
+++ 20230920/P_1260/최정윤_P_1260.java
@@ -1,15 +1,21 @@
+package baek;
 import java.io.*;
 import java.util.*;
 
 
-public class Main {
+public class Pro_1260_DFSBFS {
+	static HashMap<Integer, int[]> map;
+	static List<Integer> dfs;
+	static List<Integer> bfs;
+	static int N;
+	static Queue<Integer> queue;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int V = Integer.parseInt(st.nextToken());
-		map = new HashMap<Integer, int[]>();
+		map = new HashMap<Integer, int[]>();//1번이랑 연결되어있는 것을 key 값이 1인 map에 넣겠다 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
@@ -22,7 +28,7 @@ public class Main {
 			map.put(b, arr2);
 		} // 간선입력끝
 		queue = new LinkedList<Integer>();
-		bfs = new ArrayList<Integer>();
+		bfs = new ArrayList<Integer>();//꺼낸 순서대로 담을 리스트
 		dfs = new ArrayList<Integer>();
 		dfs.add(V);
 		dfs(V);
@@ -30,6 +36,7 @@ public class Main {
 			System.out.print(a + " ");
 		}
 		System.out.println();
+		
 		queue.add(V);
 		bfs.add(V);
 		bfs();
@@ -39,19 +46,14 @@ public class Main {
 		System.out.println();
 	}
 
-	static HashMap<Integer, int[]> map;
-	static List<Integer> dfs;
-	static List<Integer> bfs;
-	static int N;
-	static Queue<Integer> queue;
 
 	public static void dfs(int start) {
 		if (dfs.size() >= N)
 			return;
-		if (map.get(start) != null) {
+		if (map.get(start) != null) {//아무것도 연결안되어 있을 경우 대비
 			int[] arr = map.get(start);
 			for (int i = 1; i < arr.length; i++) {
-				if (arr[i] == 1 && !dfs.contains(i)) {
+				if (arr[i] == 1 && !dfs.contains(i)) {//중복 확인
 					dfs.add(i);
 					dfs(i);
 				}

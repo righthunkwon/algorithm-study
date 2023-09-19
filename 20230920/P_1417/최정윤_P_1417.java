7b8436cdbce98a91ad5719fe942cdcd6cb10d34c
jungyounn <141210401+jungyounn@users.noreply.github.com>
2023-09-20 08:33:39 +0900

    Update 최정윤_P_1417.java

--- 20230920/P_1417/최정윤_P_1417.java
+++ 20230920/P_1417/최정윤_P_1417.java
@@ -1,26 +1,26 @@
+package baek;
 import java.io.*;
 import java.util.Arrays;
 
-public class Main {
-	static int[] diff;
-	static int result;
+public class Pro_1417_선거 {
+	static int[] diff;//다솜이와 표차이 넣는 배열 
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		diff = new int[N - 1];// 다솜이 기호 빼고 다솜이랑의 표차이넣는 배열
 		int dasom = Integer.parseInt(br.readLine());
-		result = 0;
+		int result = 0;
 		for (int i = 0; i < N - 1; i++) {
 			diff[i] = Integer.parseInt(br.readLine()) - dasom;
 		}
-		while (!isfirst()) {
-			Arrays.sort(diff);
+		while (!isfirst()) {//다솜이가 1등인지 
+			Arrays.sort(diff);//차이 가장 많이 나는 사람 표부터 뺏어야함
 			for (int i = diff.length - 1; i >= 0; i--) {
-				if (diff[i] >= 0) {
-					result++;
-					diff[i] = diff[i] - 1;
-					minusAll();
+				if (diff[i] >= 0) {//표수가 다솜이랑 같거나 다솜이보다 많을 경우만
+					result++;//뺏고
+					diff[i] = diff[i] - 1;//뺏은 사람은 다솜이랑 표수가 2표 차이나기 때문에 한개 미리 빼줌
+					minusAll();//나머지 차이 -1
 					break;
 				}
 			}

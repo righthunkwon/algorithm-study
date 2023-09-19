d62e4d4b417b48c22fa65ca8aac03b92f854c8d4
ehgud619 <139290855+ehgud619@users.noreply.github.com>
2023-09-20 07:54:34 +0900

    Create 김도형_P_14500_미구현.java

---
+++ 20230920/P_14500/김도형_P_14500_미구현.java
@@ -0,0 +1,34 @@
+package gold;
+
+import java.util.Scanner;
+
+public class BaekJoon_Q14500_테트로미노 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(); //세로
+		int M = sc.nextInt(); //가로
+		
+		int [][] arr = new int [N][M];
+		
+		for(int i = 0; i<N;i++) {
+			for(int j=0;j<M;j++) {
+				
+				arr[i][j]=sc.nextInt(); //입력 끝
+				
+			}
+		}
+		
+		
+		//ㅈㅅ..
+		
+		
+		
+		
+		
+	}//main
+	
+}//class

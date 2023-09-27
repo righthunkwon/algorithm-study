0c2c11f62baa73e790043a7c4286319b8a39378d
Fezz <wlgjs0458@naver.com>
2023-09-27 12:31:17 +0900

    Update 강지헌_P_1049.java

--- 20231004/P_1049/강지헌_P_1049.java
+++ 20231004/P_1049/강지헌_P_1049.java
@@ -14,12 +14,12 @@ public class q1049 {
 			st = new StringTokenizer(br.readLine());
 			int a=Integer.parseInt(st.nextToken());
 			int b=Integer.parseInt(st.nextToken());
-			if(m6>a) m6=a;
-			if(m1>b) m1=b; 
+			if(m6>a) m6=a; //6개 구매 최소값 갱신
+			if(m1>b) m1=b; //1개 구매 최소값 갱신
 		}
 		
-		if(m1*6<m6) System.out.println(m1*N);
-		else if((N%6)*m1>m6) System.out.println(m6*(N/6+1));
-		else System.out.println(m6*(N/6)+m1*(N%6));
+		if(m1*6<m6) System.out.println(m1*N); //1개 6번 구매하는게 6개구매보다 싸면 1개로만 구매
+		else if((N%6)*m1>m6) System.out.println(m6*(N/6+1)); //6개씩 구매하고 남은걸 1개씩 구매하는것보다 6개 구매하는게 싸면 6개로만 구매
+		else System.out.println(m6*(N/6)+m1*(N%6)); //6개로 구매하고 남은건 1개로 구매하기
 	}
 }

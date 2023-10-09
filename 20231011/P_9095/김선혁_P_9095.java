033457daa15b4ec192160c3f3254602ad03ec069
slside <132821972+slside@users.noreply.github.com>
2023-10-10 01:35:13 +0900

    Create 김선혁_P_9095.java

---
+++ 20231011/P_9095/김선혁_P_9095.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		int T = sc.nextInt();
+		// N=1 -> 1개
+		// N=2 -> 2개
+		// N=3 -> 4개(1,1,1 / 1,2 / 2,1 / 3)
+		// N=4 -> 7개
+		// N=5 -> 13개 
+		// ===> N번쨰 수는 N-3 + N-2 + N-1번째 항의 합과 같다고 볼 수 있음
+			
+		
+		//N은 우선 양수이며 11보다 작기때문에
+		int[] dp = new int[12];
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		for(int i=4;i<12;i++) {
+			dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+		}
+		for(int tc=1;tc<=T;tc++) {
+			int N =sc.nextInt();
+			System.out.println(dp[N]);
+			
+		}
+		
+		
+		
+	}
+	
+
+}

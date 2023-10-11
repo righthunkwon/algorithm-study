2e0979d42e0130bc48d1a3bb3ffa603d671e1f19
hippoDev <righthun.kwon@gmail.com>
2023-10-11 17:48:02 +0900

    Update 권정훈_P_2206.java

--- 20231011/P_2206/권정훈_P_2206.java
+++ 20231011/P_2206/권정훈_P_2206.java
@@ -1,4 +1,3 @@
-package level_31_dfs_bfs;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,9 +5,6 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-
-// 참고: https://whitetigerlouis.tistory.com/33
-// 참고: https://superohinsung.tistory.com/167
 
 // 벽 부수고 이동하기
 // 최단거리 구하기이므로 bfs 사용
@@ -21,7 +17,7 @@ import java.util.StringTokenizer;
 // 벽을 부순 경우와 부수지 않은 경우의 방문배열이 꼬이지 않고 잘 해결된다.
 public class P_2206 {
 	private static int n, m, ans;
-	private static int[][] arr;
+	private static int[][] map;
 	private static int[][] dist; // 거리 측정 배열
 	private static boolean[][][] visited; // 벽을 부순 여부에 따라 방문 여부 기록
 	
@@ -32,23 +28,24 @@ public class P_2206 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        ans = -1;
-        arr = new int[n][m];
-        dist = new int[n][m];
-        visited = new boolean[2][n][m];
+        n = Integer.parseInt(st.nextToken()); // 행
+        m = Integer.parseInt(st.nextToken()); // 열
+        ans = -1; // 가지 못하는 경우로 초기화
+        map = new int[n][m]; // 지도
+        dist = new int[n][m]; // 거리
+        visited = new boolean[2][n][m]; // 방문처리배열
 
         // 시작지점과 도착지점이 같을 경우
         if (n - 1 == 0 && m - 1 == 0) {
-            System.out.print(1);
-            System.exit(0);
+            System.out.print(1); // 정답 출력후
+            System.exit(0); // 종료 
         }
 
+        // 지도 배열 요소 입력
         for (int i = 0; i < n; i++) {
-            String str = br.readLine();
+            String s = br.readLine();
             for (int j = 0; j < m; j++) {
-                arr[i][j] = str.charAt(j) - '0';
+                map[i][j] = s.charAt(j) - '0';
             }
         }
         bfs();
@@ -57,38 +54,45 @@ public class P_2206 {
 
     private static void bfs() {
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0, 0});
-        arr[0][0] = -1;
+        q.offer(new int[]{0, 0, 0}); // 시작지점을 큐에 넣음
+        map[0][0] = -1; // 시작지점 방문처리
 
         while (!q.isEmpty()) {
-            int[] poll = q.poll();
-            int x = poll[0];
-            int y = poll[1];
-            int z = poll[2]; // 지나온 경로 세기
+            int[] tmp = q.poll();
+            int x = tmp[0]; // 시작좌표
+            int y = tmp[1]; // 시작좌표
+            int z = tmp[2]; // 벽을 부쉈는지 유무(0: 안부숨, 1: 부숨)
 
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
+                // 배열 범위 벗어나면 스킵
                 if (nx < 0 || ny < 0 || n <= nx || m <= ny) {
                     continue;
                 }
 
-                if (arr[nx][ny] == 1) {
+                // 벽이 있는 경우
+                if (map[nx][ny] == 1) {
+                	// 벽을 부수지 않았고, 아직 방문하지 않았다면
                     if (z == 0 && !visited[1][nx][ny]) {
                         visited[z][nx][ny] = true; // 방문 처리
                         dist[nx][ny] = dist[x][y] + 1; // 거리 측정
                         q.offer(new int[]{nx, ny, 1});
                     }
-                } else {
+                } 
+                
+                // 벽이 없는 경우에(벽이 없었을 수도 벽을 부쉈을 수도 있음)
+                else {
+                	// 아직 방문하지 않았다면
                     if (!visited[z][nx][ny]) {
-                        // 해당 칸을 방문을 안했을 때!
                         visited[z][nx][ny] = true; // 방문 처리
                         dist[nx][ny] = dist[x][y] + 1; // 거리 측정
-                        q.offer(new int[]{nx, ny, z}); // 다시 큐에 넣어줘서 BFS!
+                        q.offer(new int[]{nx, ny, z}); // 다시 큐에 넣고 탐색
                     }
                 }
 
+                // 목적지에 도달했을 경우 누적된 거리를 더하고 리턴 
                 if (nx == n - 1 && ny == m - 1) {
                     ans = dist[nx][ny] + 1;
                     return;

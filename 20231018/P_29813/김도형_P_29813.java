96fd8a6112ec2d18cb098a7732597f13cb609d5f
ehgud619 <139290855+ehgud619@users.noreply.github.com>
2023-10-17 21:26:37 +0900

    Update 김도형_P_29813.java

--- 20231018/P_29813/김도형_P_29813.java
+++ 20231018/P_29813/김도형_P_29813.java
@@ -1,29 +1,19 @@
 package AlgoStudy;
-
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class BOJ_Q29813_최애의_팀원 {
-
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-
 		int N = sc.nextInt();
-
 		Queue<String> stu = new LinkedList<>();
 		Queue<Integer> num = new LinkedList<>();
-
 		for (int i = 0; i < N; i++) {
-
 			stu.add(sc.next());
 			num.add(sc.nextInt());
-
 		}
-
 		for (int i = 0; i < N/2; i++) {
-
 			stu.poll();
 			int a = num.poll();
 			
@@ -35,12 +25,8 @@ public class BOJ_Q29813_최애의_팀원 {
 			}
 			stu.poll();
 			num.poll();
-
-		}
-		
+		}	
 		String ans = stu.poll();
 		System.out.println(ans);
-
 	}
-
 }

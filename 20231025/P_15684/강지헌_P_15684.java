b72182697133bf022257cb2cc4866f43866e745e
Fezz <wlgjs0458@naver.com>
2023-10-25 16:49:34 +0900

    Update 강지헌_P_15684.java

--- 20231025/P_15684/강지헌_P_15684.java
+++ 20231025/P_15684/강지헌_P_15684.java
@@ -10,27 +10,25 @@ public class Main {
 		N=Integer.parseInt(st.nextToken());
 		M=Integer.parseInt(st.nextToken());
 		C=Integer.parseInt(st.nextToken());
-		arr=new int[C][N];
+		arr=new int[C+1][N+1];
 		for(int i=0;i<M;i++) {
 			st=new StringTokenizer(br.readLine());
-			arr[Integer.parseInt(st.nextToken())-1][Integer.parseInt(st.nextToken())-1]=1;
+			arr[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())]=1;
 		}
-		for(int i=0;i<4;i++) dfs(0,0,i);
+		for(int i=0;i<4;i++) dfs(1,0,i);
 		System.out.println(-1);
 	}
 	private static void dfs(int st, int c,int mc) {
 		if(c==mc) {
 			if(check()) {
-				System.out.println(c);
+				System.out.println(mc);
 				System.exit(0);
 			}
 			return;
 		}
-		for(int i=st;i<C;i++) {
-			for(int j=0;j<N-1;j++) {
-				int t=arr[i][j]+arr[i][j+1];
-				if(j-1>=0) t+=arr[i][j-1];
-				if(t==0) {
+		for(int i=st;i<=C;i++) {
+			for(int j=1;j<N;j++) {
+				if(arr[i][j]+arr[i][j+1]+arr[i][j-1]==0) {
 					arr[i][j]=1;
 					dfs(i,c+1,mc);
 					arr[i][j]=0;
@@ -39,9 +37,9 @@ public class Main {
 		}
 	}
 	static boolean check() {
-		for(int i=0;i<N;i++) {
+		for(int i=1;i<=N;i++) {
 			int t=i;
-			for(int j=0;j<C;j++) {
+			for(int j=1;j<=C;j++) {
 				if(arr[j][t]!=0) t++;
 				else if(t-1>=0 && arr[j][t-1]!=0) t--;
 			}

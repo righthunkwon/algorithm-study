63ccd9ee1722cf1ab4b7e81c57f2f94d4f680b47
ehgud619 <139290855+ehgud619@users.noreply.github.com>
2023-11-01 16:13:01 +0900

    Update 김도형_P_12100.java

--- 20231101/P_12100/김도형_P_12100.java
+++ 20231101/P_12100/김도형_P_12100.java
@@ -9,16 +9,12 @@ import java.util.StringTokenizer;
 public class BOJ_Q12100_2048_Easy {
 
 	static int N, ans;
-//	static int[][] arr;
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-
 		StringTokenizer st;
 		int [][]arr = new int[N][N];
-
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
@@ -26,40 +22,25 @@ public class BOJ_Q12100_2048_Easy {
 			}
 		}
 		// 입력끝
-
-//		for (int i = 0; i < N; i++) {
-//			System.out.println(Arrays.toString(arr[i]));
-//		}
-//
-//		moveleft(arr);
-//		System.out.println("------------이동후 ---------");
-//		for (int i = 0; i < N; i++) {
-//			System.out.println(Arrays.toString(arr[i]));
-//		}
-
 		
 		ans = 0; //답 초기화
-
-		dfs(0, arr); 
-		
+		dfs(0, arr); 	
 		System.out.println(ans);
-
-	}
+	}//main
 
 	public static void dfs(int depth, int[][] arr) {
-
 		// 기저
 		if (depth >= 5) {
 			ans = Math.max(ans, maxNum(arr));
 			return;
 		}
 
-		int[][]tmp = new int [N][N];  //배열 복사해주기.. 원본 arr 안건들도록
-		
 		// 재귀
+		int[][]tmp = new int [N][N];  //배열 복사해주기.. 원본배열 안건들도록
+		
 		for(int i=0;i<N;i++) {
 			for(int j=0;j<N;j++) {
-				tmp[i][j]=arr[i][j];
+				tmp[i][j]=arr[i][j]; 
 			}
 		}
 		dfs(depth + 1, moveup(tmp)); //위로이동
@@ -84,9 +65,7 @@ public class BOJ_Q12100_2048_Easy {
 			}
 		}
 		dfs(depth + 1, moveright(tmp)); //오른쪽이동
-		
-
-	}
+	}//dfs
 	
 	
 	/*이동하는 로직
@@ -95,8 +74,6 @@ public class BOJ_Q12100_2048_Easy {
 	 * 1. 자기가 0이면 패쓰
 	 * 2. 자기가 이동할 위치가 0이면 이동 (합쳐진 배열이면 합쳤었다는 boolean도 같이 이동)
 	 * 3. 자기가 이동할 위치가 이번 이동때 합쳐진 적 없고 자기와 같은 숫자면 합치고 합 여부 boolean도 true처리
-	 * 
-	 * 
 	 * */
 	
 
@@ -133,7 +110,6 @@ public class BOJ_Q12100_2048_Easy {
 	}
 	
 	//이하 동일한 로직으로..
-
 	// 아래로이동
 	public static int[][] movedown(int[][] arr) {
 		int L = arr.length;
@@ -143,7 +119,6 @@ public class BOJ_Q12100_2048_Easy {
 				int x = i;
 				int y = j;
 				if(arr[i][j]==0)continue;
-				
 				while (true) {
 					if (x + 1 < L && arr[x + 1][y] == 0) { // 이동
 						arr[x + 1][y] = arr[x][y];
@@ -174,7 +149,6 @@ public class BOJ_Q12100_2048_Easy {
 				int x = i;
 				int y = j;
 				if(arr[i][j]==0)continue;
-				
 				while (true) {
 					if (y - 1 >= 0 && arr[x][y - 1] == 0) { // 이동
 						arr[x][y - 1] = arr[x][y];
@@ -205,7 +179,6 @@ public class BOJ_Q12100_2048_Easy {
 				int x = i;
 				int y = j;
 				if(arr[i][j]==0)continue;
-				
 				while (true) {
 					if (y + 1 < L && arr[x][y + 1] == 0) { // 이동
 						arr[x][y + 1] = arr[x][y];
@@ -226,7 +199,7 @@ public class BOJ_Q12100_2048_Easy {
 		}
 		return arr;
 	}
-
+	
 	// 배열 내 최대값 구하기
 	public static int maxNum(int[][] arr) {
 		int max = 0;
@@ -237,5 +210,4 @@ public class BOJ_Q12100_2048_Easy {
 		}
 		return max;
 	}
-
 }

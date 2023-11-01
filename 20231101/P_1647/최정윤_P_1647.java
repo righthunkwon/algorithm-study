e9b366374fe3a2b9e0cf661d72794471aee99d48
jungyounn <141210401+jungyounn@users.noreply.github.com>
2023-11-01 16:40:50 +0900

    Update 최정윤_P_1647.java

--- 20231101/P_1647/최정윤_P_1647.java
+++ 20231101/P_1647/최정윤_P_1647.java
@@ -2,6 +2,11 @@ package baek;
 
 import java.io.*;
 import java.util.*;
+//두 집 사이에는 경로가 항상 존재 , 유지비 최소 
+//=> 프림알고리즘 사용으로 모든 선 이어지는 최소 신장 트리 고르고(문제에서 임의의 두 집 사이에 경로 항상 존재),
+//그 중 가장 max값인 선  한 개 삭제하면 마을 두 개로 분리된다.
+
+//프림알고리즘 : 노드 기준으로 방문하고 그 뒤 방문 노드와 연결된 간선을 확인하여 최소 거리 간선을 선택하는 방식으로 진행 
 public class Pro_1647_도시분할계획 {
 	static class Node implements Comparable<Node> {
 		int ed, w;
@@ -23,17 +28,14 @@ public class Pro_1647_도시분할계획 {
 	static boolean[] visited;
 	static PriorityQueue<Node> pq;
 
-	// 프림알고리즘 사용으로 모든 선 이어지는 최소 신장트리 고르고(문제에서 임의의 두집 사이에 경로 항상 존재), 그 중 가장 max값인 선
-	// 한개 삭제하면 마을 두개로 분리된다.
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		arr = new ArrayList[N + 1];// 배열안에 인접한 Node리스트 넣기,1번부터 하려고 N+!
+		arr = new ArrayList[N + 1];// 배열안에 인접한 Node리스트 넣기,1번부터 하려고 N+1
 		// 미리 모든 list 생성
-		for (int i = 0; i < N + 1; i++)
-			arr[i] = new ArrayList<Node>();
+		for (int i = 0; i < N + 1; i++) arr[i] = new ArrayList<Node>();
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int A = Integer.parseInt(st.nextToken());
@@ -44,16 +46,15 @@ public class Pro_1647_도시분할계획 {
 		} // 입력 끝
 		dist = new int[N + 1];
 		visited = new boolean[N + 1];
-		Arrays.fill(dist, INF);
+		Arrays.fill(dist, INF);// 최댓값 세팅
 		// 1번 정점부터 시작
 		pq = new PriorityQueue<Node>();
 		pq.add(new Node(1, 0));// 시작 1번 정점부터 찾는다.
 		prim();
-//		System.out.println(Arrays.toString(dist));
 		int max = 0;
 		int sum = 0;
 		for (int i = 1; i < N + 1; i++) {
-			max = Math.max(dist[i], max);
+			max = Math.max(dist[i], max);//간선 중 가장 최대 간선 자르려고 저장
 			sum += dist[i];
 		}
 		System.out.println(sum - max);
@@ -62,13 +63,13 @@ public class Pro_1647_도시분할계획 {
 	private static void prim() {
 		while (!pq.isEmpty()) {
 			Node curr = pq.poll();
-			if (!visited[curr.ed]) {
-				visited[curr.ed] = true;
-				dist[curr.ed] = curr.w;// pq로 꺼내진 가장 작은값 visited된 것은 바뀔 수없음
+			if (!visited[curr.ed]) {			//정점이 방문하지 않았다면
+				visited[curr.ed] = true;		//방문처리 
+				dist[curr.ed] = curr.w;		    // pq로 꺼내진 가장 노드는 선택된 것이므로 값 저장.
 				List<Node> list = arr[curr.ed];// 끝점을 시작점으로 하는 간선리스트
 				for (Node n : list) {
-					if (!visited[n.ed])
-						pq.add(n);// 방문이 끝난 정점이 아니면 넣어라
+					if (!visited[n.ed])// 도착지점이 방문처리 되지 않앗다면(사이클을 막기 위해), pq에 넣기
+						pq.add(n);
 				}
 
 			}

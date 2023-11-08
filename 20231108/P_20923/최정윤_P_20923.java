1a06814ca8c3060ae48c7a7c2f43bbd0916cf598
jungyounn <141210401+jungyounn@users.noreply.github.com>
2023-11-08 17:00:15 +0900

    Update 최정윤_P_20923.java

--- 20231108/P_20923/최정윤_P_20923.java
+++ 20231108/P_20923/최정윤_P_20923.java
@@ -1,7 +1,9 @@
+package baek;
+
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Pro_20923_숫자할리갈리게임 {
 	static Deque<Integer> dodo, su;
 	static Queue<Integer> doground, suground;
 	static int M, dodoCard, suCard;
@@ -22,18 +24,18 @@ public class Main {
 			dodo.addLast(Integer.parseInt(st.nextToken()));
 			su.addLast(Integer.parseInt(st.nextToken()));
 		}
-		dodoCard = 0;
-		suCard = 0;
+		dodoCard = 0;//각자 그라운드 맨 위 카드 숫자 기억해놓기
+		suCard = 0; //0일 때는 그라운드에 카드 없는 것
 		int cnt = 0;
 		win = "";
 		while (true) {
-			game1();
+			gameDoDo();//도도가 카드 한장 뒤집기
 			cnt++;
 			if (cnt == M) {
 				whoWin();
 				break;
 			}
-			game2();
+			gameSu();
 			cnt++;
 			if (cnt == M) {
 				whoWin();
@@ -52,23 +54,24 @@ public class Main {
 			win = "do";
 	}
 
-	private static void game1() {
-		dodoCard = dodo.peekLast();
-		doground.add(dodo.removeLast());
-		if (dodo.size() == 0) {
-			System.out.println("su");
+	private static void gameDoDo() {
+		dodoCard = dodo.peekLast();//도도 맨 위 카드 뒤집어서 그라운드 맨 위 카드 값 저장시키기
+		doground.add(dodo.removeLast());//그라운드에 넣기
+		if (dodo.size() == 0) {//그라운드에 놓았더니 카드가 없을 때 => 패
+			System.out.println("su"); //su가 이김
 			System.exit(0);
 		}
-		if (dodoCard != 0 && suCard != 0 && dodoCard + suCard == 5) {
+		if (dodoCard != 0 && suCard != 0 && dodoCard + suCard == 5) {//양쪽 그라운드에 모두 카드가 있고, 맨 위 카드 합이 5이면 수연이가 카드 가져감 
 			// 수연이가 가져감
 			jong(doground, suground, su);// 상대 그라운드, 본인 그라운드, 본인 카드더미
 
-		} else if (dodoCard == 5) {
+		} else if (dodoCard == 5) {//내가 방금 뒤집은 것이 5일 때, 
+			//도도가 가져감, 종을 친다.
 			jong(suground, doground, dodo);
 		}
 	}
 
-	private static void game2() {
+	private static void gameSu() {//사람만 바뀌고 같은 방식
 		suCard = su.peekLast();
 		suground.add(su.removeLast());
 		if (su.size() == 0) {
@@ -76,6 +79,7 @@ public class Main {
 			System.exit(0);
 		}
 		if (dodoCard != 0 && suCard != 0 && dodoCard + suCard == 5) {
+			//수연이가 가져감
 			jong(doground, suground, su);
 		} else if (suCard == 5) {
 			// 도도가 가져감
@@ -84,14 +88,14 @@ public class Main {
 
 	}
 
-	private static void jong(Queue<Integer> youground, Queue<Integer> myground, Deque<Integer> myCard) {
-		while (!youground.isEmpty()) {
+	private static void jong(Queue<Integer> youground, Queue<Integer> myground, Deque<Integer> myCard) {//상대 그라운드, 내 그라운드, 내 카드더미
+		while (!youground.isEmpty()) {//상대 그라운드의 바닥에 깔린 것부터 내 카드 아래로 넣는다.(뒤집어져 있으니까)
 			myCard.addFirst(youground.poll());
 		}
-		while (!myground.isEmpty()) {
+		while (!myground.isEmpty()) {//그다음 내 그라운드에 있는 것 내 카드 아래로 넣는다. 
 			myCard.addFirst(myground.poll());
 		}
-		dodoCard = 0;
+		dodoCard = 0;//넣고 나면 그라운드 맨 위 카드숫자 없다고 초기화.
 		suCard = 0;
 	}
 }

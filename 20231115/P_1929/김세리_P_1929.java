afca87246152e111b2afd9b341dcdecf092f6a03
onid057 <141606477+onid057@users.noreply.github.com>
2023-11-15 16:47:13 +0900

    Update 김세리_P_1929.java

--- 20231115/P_1929/김세리_P_1929.java
+++ 20231115/P_1929/김세리_P_1929.java
@@ -13,6 +13,8 @@ public class _1929_소수구하기 {
         
         boolean[] prime = new boolean[N+1];
         prime[0]=prime[1]=true;
+        // 그냥 풀면 시간초과나서
+        // 에라토스테네스의 체를 이용해서 구해야 한다
         for(int i=2;i*i<N+1;i++) {
         	if(!prime[i]) {
         		for(int j=i*i;j<=N;j+=i) {
@@ -24,6 +26,7 @@ public class _1929_소수구하기 {
         	if(!prime[i]) System.out.println(i);
         }
 
+        //시간초과 코드ㅜㅜ
 //        for(int i=M;i<=N;i++) {
 //        	int cnt=0;
 //        	out: for(int j=2;j<=N;j++) {

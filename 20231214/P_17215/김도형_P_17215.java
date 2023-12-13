02921d4cad7e2fc4e34a57633e2ddcb6bb2926bc
ehgud619 <139290855+ehgud619@users.noreply.github.com>
2023-12-14 01:43:30 +0900

    Update 김도형_P_17215.java

--- 20231214/P_17215/김도형_P_17215.java
+++ 20231214/P_17215/김도형_P_17215.java
@@ -3,18 +3,18 @@ package AlgoStudy;
 import java.util.Arrays;
 import java.util.Scanner;
 
+//그림 링크
+//https://postimg.cc/34fkYJLd/bd72fd4b
 public class BOJ_Q17215_볼링점수계산 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
 		char[]arr = str.toCharArray();
-		
 		int [] pin = new int[26]; //기본 점수 기록할 배열
 		int [] spareBonus = new int [26]; //스페어 보너스점수 기록할 배열
 		int [] strikeBonus = new int [26]; //스트라이크 보너스점수 기록할 배열
-		
 		int idx =0;
-		//기본 핀 개수 배열에 넣기(단, 스트라이크치면 10, -1을 넣어줌 why? 
+		//기본 핀 개수 배열에 넣기(단, 스트라이크치면 10, -1을 넣어줌(칸수 맞추기 위해))
 		for(int i=0;i<str.length();i++) {
 			if(arr[i]=='S') {//스트라이크
 				pin[idx++]=10;
@@ -28,16 +28,12 @@ public class BOJ_Q17215_볼링점수계산 {
 				pin[idx++]=arr[i]-'0';
 			}
 		}
-//		System.out.println(Arrays.toString(pin));
-
 		//스페어 보너스 계산
 		for(int i=0;i<24;i++) {
 			if(i%2==1 && pin[i]+pin[i-1]==10) { //스페어면
 				spareBonus[i]=pin[i+1];
 			}
 		}
-//		System.out.println(Arrays.toString(spairBonus));
-		
 		//스트라이크 보너스 계산
 		for(int i=0;i<24;i++) {
 			//i%2==0 -> 거터치고 스페어 처리한거랑 구분하기 위해서
@@ -47,17 +43,13 @@ public class BOJ_Q17215_볼링점수계산 {
 				strikeBonus[i]=pin[i+2]+pin[i+4];
 			}
 		}
-//		System.out.println(Arrays.toString(strikeBonus));
-		
 		//총점 계산
 		int total=0;
-		for(int i=0;i<20;i++) {
-			if(pin[i]>=0)total+=pin[i];
+		for(int i=0;i<20;i++) { //20번째부터는 스페어,스트라이크 보너스 기록은 되지만 더하지 않으면 됨
+			if(pin[i]>=0)total+=pin[i]; //스트라이크 뒤에 임의로 넣어둔 -1은 더하면 안되니까
 			total+=spareBonus[i];
 			total+=strikeBonus[i];
 		}
-		
 		System.out.println(total);
-		
 	}
 }

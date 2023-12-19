b1b5e58b2af205b28ced18f7135f33b1e07ec5c8
Fezz <wlgjs0458@naver.com>
2023-12-19 19:14:18 +0900

    Create 강지헌_P_2666.java

---
+++ 20231220/P_2666/강지헌_P_2666.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int[] brr;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int a=Integer.parseInt(st.nextToken()),b=Integer.parseInt(st.nextToken());
+		
+		int m=Integer.parseInt(br.readLine());
+		brr=new int[m];
+		for(int i=0;i<m;i++) brr[i]=Integer.parseInt(br.readLine());
+		
+		System.out.println(solve(0,a,b));
+	}
+	static int solve(int cnt,int a,int b) {
+		if(cnt == brr.length) return 0;
+		return Math.min(Math.abs(a-brr[cnt])+solve(cnt+1,b,brr[cnt]),Math.abs(b-brr[cnt])+solve(cnt+1,a,brr[cnt]));
+	}
+}

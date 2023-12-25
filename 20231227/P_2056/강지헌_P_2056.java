49f49f626a854a5f6d174c340986c0378de99b2b
Fezz <wlgjs0458@naver.com>
2023-12-25 22:20:38 +0900

    Create 강지헌_P_2056.java

---
+++ 20231227/P_2056/강지헌_P_2056.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int ans=0;
+		int N=Integer.parseInt(br.readLine());
+		int[] dy=new int[N+1];
+		for(int i=1;i<=N;i++) {
+			StringTokenizer st=new StringTokenizer(br.readLine());
+			int a=Integer.parseInt(st.nextToken());
+			int b=Integer.parseInt(st.nextToken());
+			dy[i]=a;
+			for(int j=0;j<b;j++) {
+				int t=Integer.parseInt(st.nextToken());
+				dy[i]=Math.max(dy[i],dy[t]+a);
+			}
+			ans=Math.max(ans,dy[i]);
+		}
+		System.out.println(ans+"\n");
+	}
+}

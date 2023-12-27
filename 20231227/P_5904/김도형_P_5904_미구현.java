ac09be9307d3773595de0721e21a330dfa4989f8
ehgud619 <139290855+ehgud619@users.noreply.github.com>
2023-12-27 16:34:20 +0900

    Create 김도형_P_5904_미구현.java

---
+++ 20231227/P_5904/김도형_P_5904_미구현.java
@@ -0,0 +1,30 @@
+package AlgoStudy;
+
+import java.util.Scanner;
+
+public class BOJ_Q5904_Moo게임 {
+	
+	static char ans;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		
+	}//main
+	
+	//재귀함수
+	public static void Moo(int x) {
+		int size = 3;
+		int idx = 0;
+		
+		if(x==1) {
+			ans='m';
+		}else if(x<=3) {
+			ans='o';
+		}else {
+			
+		}
+		
+	}
+	
+}//class

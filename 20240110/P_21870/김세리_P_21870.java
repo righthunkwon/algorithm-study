c70f6c9dcf1d13509f9557fecf0e001db6fbbede
onid057 <141606477+onid057@users.noreply.github.com>
2024-01-10 17:59:28 +0900

    Update 김세리_P_21870.java

--- 20240110/P_21870/김세리_P_21870.java
+++ 20240110/P_21870/김세리_P_21870.java
@@ -3,6 +3,8 @@ package _20240110;
 import java.io.*;
 import java.util.*;
 
+// https://hbj0209.tistory.com/105 파이썬 코드 참고함.
+
 public class _21870_시철이가사랑한GCD {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,11 +28,13 @@ public class _21870_시철이가사랑한GCD {
         } else if (array.length == 2) {
             return array[0] + array[1];
         } else {
+        	// 근데 이부분을 이해못함...ㅜㅜ
             return Math.max(solve(Arrays.copyOfRange(array, 0, mid)) + gcd(Arrays.copyOfRange(array, mid, array.length)),
             		solve(Arrays.copyOfRange(array, mid, array.length)) + gcd(Arrays.copyOfRange(array, 0, mid)));
         }
     }
-
+    
+    // 여러개에서 gcd구하기
     private static int gcd(int[] lst) {
         int a = lst[0];
         for (int i = 1; i < lst.length; i++) {
@@ -38,7 +42,8 @@ public class _21870_시철이가사랑한GCD {
         }
         return a;
     }
-
+    
+    // 유클리드 호제법
     private static int gcd(int x, int y) {
         while (y != 0) {
             int temp = y;

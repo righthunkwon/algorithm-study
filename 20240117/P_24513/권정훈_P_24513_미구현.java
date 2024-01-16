e0dbe33d580929db365e83c49f1763d91c2e8424
hippoDev <righthun.kwon@gmail.com>
2024-01-17 00:53:09 +0900

    Create 권정훈_P_24513.java

---
+++ 20240117/P_24513/권정훈_P_24513_미구현.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+// 좀비 바이러스
+public class P_24513 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] map = new int[M][N];
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+	}
+}

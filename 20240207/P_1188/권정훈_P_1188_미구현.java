5d69752421b69517995c8aed77c1553343294ef6
hippoDev <righthun.kwon@gmail.com>
2024-02-07 01:50:14 +0900

    Create 권정훈_P_1188.java

---
+++ 20240207/P_1188/권정훈_P_1188_미구현.java
@@ -0,0 +1,13 @@
+
+import java.io.*;
+import java.util.*;
+
+// 음식 평론가
+public class P_1188 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+	}
+}

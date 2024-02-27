370d34ff23a84fabb1cf1253d959198de4a013f8
Fezz <wlgjs0458@naver.com>
2024-02-27 14:22:13 +0900

    Create 강지헌_P_23337.java

---
+++ 20240228/P_23337/강지헌_P_23337.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(1.0/(n-1)+(double)(n-2)/(n-1)/2);
+    }
+}

c2de8f1d7e6df4564443b76c9aa4c1b79d0a3fd4
Dohyeong Kim <139290855+ehgud619@users.noreply.github.com>
2024-02-28 18:09:23 +0900

    Update 김도형_P_16943.java

--- 20240228/P_16943/김도형_P_16943.java
+++ 20240228/P_16943/김도형_P_16943.java
@@ -58,13 +58,10 @@ import java.util.Scanner;
 public class BOJ_Q16943_숫자_재배치_반례찾기실패 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-//		int C = sc.nextInt();
-//		int D = sc.nextInt();
-//		String A = String.valueOf(C);
-//		String B = String.valueOf(D);
-		String A = sc.next();
-		String B = sc.next();
-		
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+		String A = String.valueOf(C);
+		String B = String.valueOf(D); //입력으로 앞에 0 들어오는 경우 
 		
 		int L = A.length();
 		int [] arr = new int[L]; 
@@ -73,9 +70,8 @@ public class BOJ_Q16943_숫자_재배치_반례찾기실패 {
 			arr[i]=A.charAt(i)-'0';
 		}
 		Arrays.sort(arr); //오름차순 정렬
-		//B가 자릿수가 더 크면 arr 뒤에서부터 출력하면 됨
-		//A 자릿수가 더 크면 -1출력
-		//A 0으로 시작 불가 
+
+		
 		if(L>B.length()) {
 			System.out.println(-1);
 			return;
@@ -86,7 +82,7 @@ public class BOJ_Q16943_숫자_재배치_반례찾기실패 {
 			}
 			return;
 		}
-    
+
 		StringBuilder answer = new StringBuilder();
 		boolean ispossible = false;
 		StringBuilder tmp = new StringBuilder();
@@ -100,12 +96,15 @@ public class BOJ_Q16943_숫자_재배치_반례찾기실패 {
 				break;
 			}
 		}
+		
 		for(int i=0;i<L;i++) {
 			if(!tmpUsed[i])tmp.append(arr[i]);
 		}
-//		System.out.println("최소:"+tmp.toString());
 		if(Integer.parseInt(tmp.toString())<Integer.parseInt(B)) {
 			ispossible=true;
+		}else {
+			System.out.println(-1);
+			return;
 		}
 		
 		//길이가 동일한 경우
@@ -116,14 +115,9 @@ public class BOJ_Q16943_숫자_재배치_반례찾기실패 {
 			}
 			boolean []tmpbool = new boolean[L];//@@@@@@@@@@@@@@@@@여기 확인중
 			for(int i=0;i<L;i++) { //b인덱스
-//				System.out.println("ㅎㅇ");
 				z:for(int j=L-1;j>=0;j--) { //a 배열 인덱스
-//					System.out.println("nowj="+j + "숫자:"+arr[j]);
 					if(used[j])continue; //이미 쓴거 패쓰
-//					System.out.println("B:"+ (B.charAt(i)-'0'));
-//					System.out.println("arr[j]:"+arr[j]);
 					if(B.charAt(i)-'0'>arr[j]) { //A보다 B가 무조건 큰 경우
-//						System.out.println("//A보다 B가 무조건 큰 경우");
 						used[j]=true;
 						answer.append(arr[j]);
 						for(int k=L-1;k>=0;k--) {
@@ -134,33 +128,23 @@ public class BOJ_Q16943_숫자_재배치_반례찾기실패 {
 					}else if(B.charAt(i)-'0'==arr[j]) { //뒷자리 더 봐야..(뒷자리 나머지 최소 vs B뒷자리)=> B가 클 수 없으면 -1출력
 						StringBuilder sbA = new StringBuilder();//임시용
 						
-//						System.out.println("ans"+answer.toString());////////////////////
 						sbA.append(answer.toString());
 						sbA.append(arr[j]);
 						tmpbool[j]=true;
-//						System.out.println("악"+sbA.toString());
 						for(int k=0;k<L;k++) {
-//							System.out.println("슈우웃");
 							if(!tmpbool[k] && k!=j )sbA.append(arr[k]); //사용안했던것들 작은거부터 다 합쳐
-//							System.out.println(sbA.toString());
 						}
-//						System.out.println("최소A"+Integer.parseInt(sbA.toString()));
-//						System.out.println("B"+Integer.parseInt(B));
-						if(Integer.parseInt(sbA.toString())>=Integer.parseInt(B)) { //만들수 있는 최소가 B보다 크면 -1출력, 끝
-//							continue;
+						if(Integer.parseInt(sbA.toString())>=Integer.parseInt(B)) { 
 							if(!ispossible) { //불가능한 상태 판단 완료
-//								System.out.println("ddd");
 								System.out.println(-1);
 								return;
 							}else { //현재 arr[j]보다 작은 수를 이자리에 넣고, 나머지 큰 순서대로 넣어서 출력하기
-//								System.out.println("합치자"+answer.toString());
 								int idx = j;
 								while(arr[idx]==arr[j] || used[idx]) {
 									idx--;
 								}
 								used[idx]=true;
 								answer.append(arr[idx]);
-//								System.out.println("합치자"+answer.toString());
 								for(int k=L-1;k>=0;k--) {
 									if(!used[k])answer.append(arr[k]); //사용안했던것들 다 합쳐
 								}
@@ -172,9 +156,7 @@ public class BOJ_Q16943_숫자_재배치_반례찾기실패 {
 						//B보다 작은수 만들 수 있으면 => 
 						else { //
 							used[j]=true;
-//							System.out.println("왓냐");
 							answer.append(arr[j]);
-//							System.out.println("현재답:"+answer.toString());
 							ispossible = true; //-1은 출력하지 않도록
 							break z;
 						}
@@ -183,7 +165,6 @@ public class BOJ_Q16943_숫자_재배치_반례찾기실패 {
 					}
 				}//for j
 			}//for i
-			
 			System.out.println(answer.toString());
 		}
 	}//main

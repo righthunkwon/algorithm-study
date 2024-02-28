b09169b22c81cbc046f68cef005ee89eb764e76f
hippoDev <righthun.kwon@gmail.com>
2024-02-28 09:04:25 +0900

    Update 권정훈_P_23337.java

--- 20240228/P_23337/권정훈_P_23337.java
+++ 20240228/P_23337/권정훈_P_23337.java
@@ -1,13 +1,20 @@
-package level_23_dynamic_programming;
 
 import java.io.*;
 import java.util.*;
 
 // Drunk passenger
 public class P_23337 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		System.out.println(1.0 / (n - 1) + (double) (n - 2) / (n - 1) / 2);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        double[] dp = new double[N + 1];
+        dp[N] = (double) 1 / (N - 1);
+        for (int i = N - 1; i > 1; i--) {
+            // dp[i] : 확률 누적 결과
+            // dp[i + 1] : 이전 사람이 내 자리에 앉을 확률
+            // dp[i + 1] * ( 1 / i ) : 해당 사람이 내 자리에 앉을 확률
+            dp[i] = dp[i + 1] + dp[i + 1] * (double) 1 / i;
+        }
+        System.out.print(dp[2]);
+    }
 }

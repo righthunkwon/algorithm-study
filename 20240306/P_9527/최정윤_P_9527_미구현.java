f5d540a9c0755b6e5bbe06fdc9b6a7455a3b3e1d
jungyounn <141210401+jungyounn@users.noreply.github.com>
2024-03-06 17:03:30 +0900

    Create 최정윤_P_9527_미구현.java

---
+++ 20240306/P_9527/최정윤_P_9527_미구현.java
@@ -0,0 +1,13 @@
+package study_240306;
+
+import java.util.*;
+
+public class Pro_9527_1의개수세기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Long A = sc.nextLong();
+		Long B = sc.nextLong();
+
+
+	}
+}

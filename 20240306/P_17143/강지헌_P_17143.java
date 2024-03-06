3bcb2a8caa5306401fe2a584362951b17730c45d
Fezz <wlgjs0458@naver.com>
2024-03-06 09:45:28 +0900

    Update and rename 강지헌_P_17143_미구현.java to 강지헌_P_17143.java

--- 20240306/P_17143/강지헌_P_17143.java
+++ 20240306/P_17143/강지헌_P_17143.java
@@ -14,6 +14,7 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
         int[][] arr = new int[N][M];
+        int[] xx={-1,1,0,0},yy={0,0,1,-1};
         List<Node> Q = new ArrayList<>();
 
         for (int i = 0; i < C; i++) {
@@ -23,7 +24,7 @@ public class Main {
             int c = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            Q.add(new Node(a - 1, b - 1, c, d, e));
+            Q.add(new Node(a - 1, b - 1, c, d-1, e));
         }
         Q.sort((o1, o2) -> {
             return o2.z - o1.z;
@@ -31,7 +32,7 @@ public class Main {
         int ans = 0;
         for (int i = 0; i < M; i++) {
             int min = Integer.MAX_VALUE, mx = -1;
-            for (int j = 0; j < C; j++) {
+            for (int j = 0; j < Q.size(); j++) {
                 if (Q.get(j).y == i && min > Q.get(j).x) {
                     min = Q.get(j).x;
                     mx = j;
@@ -41,13 +42,29 @@ public class Main {
                 ans += Q.get(mx).z;
                 Q.remove(mx);
             }
-            boolean[] chk = new boolean[C];
+            boolean[] chk = new boolean[Q.size()];
+            boolean[][] map = new boolean[N][M];
             for (int j = 0; j < Q.size(); j++) {
                 Node t = Q.get(j);
-
+                int dx=t.x+xx[t.d]*(t.s%((N-1)*2)),dy=t.y+yy[t.d]*(t.s%((M-1)*2));
+                while(dx<0 || dx>=N) {
+                    if(dx<0) { t.d=1-t.d; dx=Math.abs(dx); }
+                    if(dx>=N) { t.d=1-t.d; dx = (N*2-2)-dx; }
+                }
+                while(dy<0 || dy>=M) {
+                    if(dy<0) { t.d=5-t.d; dy=Math.abs(dy); }
+                    if(dy>=M) { t.d=5-t.d; dy = (M*2-2)-dy; }
+                }
+                if(map[dx][dy]) chk[j]=true;
+                map[dx][dy]=true;
+                t.x=dx; t.y=dy;
+                Q.set(j,t);
             }
-
+            for(int j=Q.size()-1;j>=0;j--) {
+                if(chk[j]) Q.remove(j);
+            }
         }
+        System.out.println(ans);
     }
 }
 

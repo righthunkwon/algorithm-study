8d585296af5ca8e7b2e61bd81840374a09effd75
Kim SunHyeok <132821972+sunhyeok99@users.noreply.github.com>
2024-03-26 22:05:50 +0900

    Create 김선혁_P_2629_미구현.java

---
+++ 20240327/P_2629/김선혁_P_2629_미구현.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int N;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		boolean[] arr = new boolean[40001];
+		// 하나씩 누적합 구하면서
+		// true인 지점에는 해당 숫자 +한값 -한값 true로 바꿔줌
+		arr[sc.nextInt()] = true;
+		for(int i = 0 ;i<N-1;i++) {
+			
+		}
+		
+		
+	}
+
+}

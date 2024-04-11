d1c36dcce907592c0064ef86db0242dc3bb2ca79
Dohyeong Kim <139290855+ehgud619@users.noreply.github.com>
2024-04-11 12:25:14 +0900

    Create 김도형_P_2629_미구현.java

---
+++ 20240327/P_2629/김도형_P_2629_미구현.java
@@ -0,0 +1,23 @@
+package AlgoStudy;
+
+import java.util.Scanner;
+
+public class BOJ_Q2629_양팔저울 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(); //추의 개수
+		int [] choo = new int[N];
+		for(int i=0;i<N;i++) {
+			choo[i]=sc.nextInt();
+		}
+		
+		int M = sc.nextInt(); //무게 수
+		int [] weight = new int[M];
+		for(int i=0;i<M;i++) {
+			weight[i]=sc.nextInt();
+		}
+		
+		
+		
+	}//main
+}//class

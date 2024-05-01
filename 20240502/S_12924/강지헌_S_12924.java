0429cacd12b2971bfb2fa6d2ed56cf760a4499da
Fezz <wlgjs0458@naver.com>
2024-05-02 08:47:46 +0900

    Update 강지헌_S_12924.java

--- 20240502/S_12924/강지헌_S_12924.java
+++ 20240502/S_12924/강지헌_S_12924.java
@@ -1,13 +1,12 @@
 class Solution {
-    public String solution(String s) {
-        String answer = "";
-        char[] t = s.toCharArray();
-        if(t[0]>='a' && t[0]<='z') t[0]-=32;
-        for(int i=1;i<t.length;i++) {
-            if(t[i]==' ' && i!=t.length-1 && t[i+1]>='a' && t[i+1]<='z') t[i+1]-=32;
-            else if(t[i-1]!=' ' && t[i]>='A' && t[i]<='Z') t[i]+=32;
+    public int solution(int n) {
+        int answer = 0;
+        int s=1,e=1,m=1;
+        while(s<=n) {
+            if(m==n) answer++;
+            if(m>n) m-=s++;
+            else m+=++e;
         }
-        for(int i=0;i<t.length;i++) answer+=t[i];
         return answer;
     }
 }

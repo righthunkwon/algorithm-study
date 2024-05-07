d81b27255395af90646310da7909608d5eabb104
jungyounn <141210401+jungyounn@users.noreply.github.com>
2024-05-08 06:07:48 +0900

    Update 최정윤_S_72411.java

--- 20240502/S_72411/최정윤_S_72411.java
+++ 20240502/S_72411/최정윤_S_72411.java
@@ -2,9 +2,7 @@ import java.io.*;
 import java.util.*;
 class Solution {
     public String[] solution(String[] orders, int[] course) {
-           String[] answer;
-       
-       
+        String[] answer;
         PriorityQueue<String> pq = new PriorityQueue<>();
         for(int cnt : course) { //코스로 만들고 싶어하는 개수가 담긴 배열
             max= 0;
@@ -14,8 +12,8 @@ class Solution {
         		Arrays.sort(order_ch); 
         		selected = new boolean[order_ch.length]; 
         		select(order_ch,0,0,cnt); 
-        	}
-        	for(String str:map.keySet()){
+            }
+           for(String str:map.keySet()){
                 if(map.get(str)==max&&max>1) pq.add(str);
             }
         }
@@ -26,21 +24,20 @@ class Solution {
         return answer;
     }
 	public static Map<String,Integer> map;
-    public static boolean[] selected;
-    static int max;
-	public static void select(char[] arr, int start, int idx, int r){
-		
-        if(idx==r){
+    	public static boolean[] selected;
+   	static int max;
+	
+	public static void select(char[] arr, int start, int idx, int r){	
+        	if(idx==r){
 			String str = "";
 			for(int i = 0; i < arr.length; i++){
 				if(selected[i]) str += String.valueOf(arr[i]);
 			}
-            int cnt=map.getOrDefault(str,0)+1;
-            map.put(str,cnt);
-            max=Math.max(max,cnt);
+            		int cnt=map.getOrDefault(str,0)+1;
+            		map.put(str,cnt);
+            		max=Math.max(max,cnt);
 			return;
 		}
-		
 		for(int i = start; i < arr.length; i++){
 			if(!selected[i]){
 				selected[i] = true;

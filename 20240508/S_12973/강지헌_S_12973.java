0194140b1b98728fa2965bc36e9d8b0155e64a3d
Fezz <wlgjs0458@naver.com>
2024-05-08 16:54:49 +0900

    Update 강지헌_S_12973.java

--- 20240508/S_12973/강지헌_S_12973.java
+++ 20240508/S_12973/강지헌_S_12973.java
@@ -3,11 +3,11 @@ class Solution
 {
     public int solution(String s)
     {
-		Stack<Character> st = new Stack<Character>();
-		for(char c : s.toCharArray()) {
-			if(!st.isEmpty() && st.peek()==c) st.pop();
-			else st.push(c);
-		}
-		return st.isEmpty()?1:0;
+	Stack<Character> st = new Stack<Character>();
+	for(char c : s.toCharArray()) {
+		if(!st.isEmpty() && st.peek()==c) st.pop();
+		else st.push(c);
+	}
+	return st.isEmpty()?1:0;
     }
 }

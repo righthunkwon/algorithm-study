8a9cf539b74ae48900a6696c5b477769db7f0e79
Fezz <wlgjs0458@naver.com>
2024-05-08 16:52:28 +0900

    Create 강지헌_S_12973.java

---
+++ 20240508/S_12973/강지헌_S_12973.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+class Solution
+{
+    public int solution(String s)
+    {
+		Stack<Character> st = new Stack<Character>();
+		for(char c : s.toCharArray()) {
+			if(!st.isEmpty() && st.peek()==c) st.pop();
+			else st.push(c);
+		}
+		return st.isEmpty()?1:0;
+    }
+}

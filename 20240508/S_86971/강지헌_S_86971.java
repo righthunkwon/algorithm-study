b8ba134f8b5825a2bc4c08feecac54afb4430df8
Fezz <wlgjs0458@naver.com>
2024-05-08 17:02:40 +0900

    Create 강지헌_S_86971.java

---
+++ 20240508/S_86971/강지헌_S_86971.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    int[][] map;
+    boolean[] vi;
+    int[] c;
+    public int solution(int n, int[][] wires) {
+        int ans = n;
+        map = new int[n][n];
+        vi = new boolean[n];
+        c = new int[n];
+        Arrays.fill(c, 1);
+        for(int[] t : wires) map[t[0]-1][t[1]-1] = map[t[1]-1][t[0]-1]=1;
+        dfs(0);
+        for(int i : c) ans = Math.min(ans, Math.abs(n - 2 * i));
+        return ans;
+    }
+    private int dfs(int t) {
+        vi[t] = true;
+        for(int i = 0; i < map.length; ++i) {
+            if(vi[i] || map[t][i] == 0) continue;
+            c[t] += dfs(i);
+        }
+        return c[t];
+    }
+}

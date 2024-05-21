b176d4acd1036cbd5fc929c5a7f7f22787bc3fd1
Fezz <wlgjs0458@naver.com>
2024-05-22 08:56:54 +0900

    Create 강지헌_S_12913.java

---
+++ 20240522/S_12913/강지헌_S_12913.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    int solution(int[][] land) {
+        int n = land.length;
+        int[][] dp = new int[n+1][4];
+        for(int i=1;i<=n;i++) {
+            for(int j=0;j<4;j++) dp[i][j]=land[i-1][j];         
+        }
+        for(int i = 1; i < n+1; i++){
+            for(int j=0;j<4;j++) dp[i][j]+=Math.max(Math.max(dp[i-1][(i+1)%4],dp[i-1][(i+2)%4]),dp[i-1][(i+3)%4]);
+        }
+        Arrays.sort(dp[n]);
+        return dp[n][3];
+    }
+}

808aea87f7029e52254edd98bad1eb42a5d539c0
Fezz <wlgjs0458@naver.com>
2024-05-22 08:58:15 +0900

    Update 강지헌_S_12913.java

--- 20240522/S_12913/강지헌_S_12913.java
+++ 20240522/S_12913/강지헌_S_12913.java
@@ -7,7 +7,7 @@ class Solution {
             for(int j=0;j<4;j++) dp[i][j]=land[i-1][j];         
         }
         for(int i = 1; i < n+1; i++){
-            for(int j=0;j<4;j++) dp[i][j]+=Math.max(Math.max(dp[i-1][(i+1)%4],dp[i-1][(i+2)%4]),dp[i-1][(i+3)%4]);
+            for(int j=0;j<4;j++) dp[i][j]+=Math.max(Math.max(dp[i-1][(j+1)%4],dp[i-1][(j+2)%4]),dp[i-1][(j+3)%4]);
         }
         Arrays.sort(dp[n]);
         return dp[n][3];

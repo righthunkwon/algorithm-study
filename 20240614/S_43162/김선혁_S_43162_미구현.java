c55f2ac55582e850365c45d7083120c5c0b88cec
Kim SunHyeok <132821972+sunhyeok99@users.noreply.github.com>
2024-06-14 17:06:00 +0900

    Create 김선혁_S_43162_미구현.java

---
+++ 20240614/S_43162/김선혁_S_43162_미구현.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        
+        // 시작점을 기준으로 하나의 부모노드에 체크를하고
+        // 전체 부모노드 한번씩 돌아서 
+        // 전체 스캔해서 개수 count
+        int[] parent = new int[computers.length];
+        for(int i = 0;i)
+        
+        
+        
+        return answer;
+    }
+}

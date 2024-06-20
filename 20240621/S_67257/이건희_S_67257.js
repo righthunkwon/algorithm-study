4c17974c8a26deff4caef92673c6d232da91d705
LEEKH109 <dlrjsgml109@gmail.com>
2024-06-20 13:32:12 +0900

    [건희] 수식최대화(JavaScript)

---
+++ 20240621/S_67257/이건희_S_67257.js
@@ -0,0 +1,67 @@
+// 100-200*300-500+20
+// [100,200,300,500,20]
+// [-,*,-,+]
+// [100,60000,null,500,20]
+// [-,null,-,+]
+// [-59900,null,null,500,20]
+// [null,null,-,+]
+// [-59900,null,null,520,null]
+// [null,null,-,+]
+// [-60420,null,null,null,null]
+// [null,null,null,null]
+// 연산자 배열이 전부 null이므로
+// 결과 값 Math.abs(-60420) = 60420 출력
+
+// 결론 dfs로 풀기
+
+function solution(expression) {
+    // 1단계 숫자와 연산자 배열로 분리 => match 함수를 쓰는 법도 있다.
+    const nums = expression.split(/[*+-]/).filter(v => /\d/.test(v)).map(Number);
+    const ops = expression.split(/\d+/).filter(v => /\D/.test(v));
+    return dfs(nums, ops);
+}
+
+function calculate(a, b, operator) {
+    // 2단계 연산자 어떻게 작동할지 설정
+    switch (operator) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+    }
+}
+
+function dfs(nums, ops) {
+    if (ops.length === 0) {
+        return Math.abs(nums[0]);
+    }
+    let max = 0;
+
+    const uniqueOps = Array.from(new Set(ops));
+    for (let i = 0; i < uniqueOps.length; i++) {
+        const operator = uniqueOps[i];
+        const newNums = [];
+        const newOps = [];
+        let tempNums = [...nums];
+        let tempOps = [...ops];
+
+        for (let j = 0; j < tempOps.length; j++) {
+            if (tempOps[j] === operator) {
+                tempNums[j + 1] = calculate(tempNums[j], tempNums[j + 1], operator);
+                // 수도에서는 전위로 했는데 반복문 구조 상 후위로
+                tempNums[j] = null;
+            } else {
+                newOps.push(tempOps[j]);
+            }
+        }
+        for (let num of tempNums) {
+            if (num !== null) {
+                newNums.push(num);
+            }
+        }
+        const result = dfs(newNums, newOps);
+        if (result > max) {
+            max = result;
+        }
+    }
+    return max;
+}

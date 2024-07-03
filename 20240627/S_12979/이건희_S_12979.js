890a63df8e850c8dc68b035649184cd062f8bf7a
LEEKH109 <dlrjsgml109@gmail.com>
2024-07-03 17:02:19 +0900

    [건희] 기지국 설치(JavaScript)

---
+++ 20240627/S_12979/이건희_S_12979.js
@@ -0,0 +1,23 @@
+// 효율성 테스트 2번에서 터짐
+function solution(n, stations, W) {
+    let result = 0;
+    let coverage = 2 * W + 1;
+    let start = 1;
+
+    // forEach
+    stations.forEach(station => {
+        let leftEdge = station - W;
+        if (start < leftEdge) {
+            let uncovered = leftEdge - start;
+            result += Math.ceil(uncovered / coverage);
+        }
+        start = station + W + 1;
+    });
+
+    if (start <= n) {
+        let uncovered = n - start + 1;
+        result += Math.ceil(uncovered / coverage);
+    }
+
+    return result;
+}

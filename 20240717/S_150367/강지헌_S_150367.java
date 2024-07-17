2e3361fc611830cf31f8efab7e4af08fefdc5649
Fezz <wlgjs0458@naver.com>
2024-07-17 15:39:59 +0900

    Update 강지헌_S_150367.java

--- 20240717/S_150367/강지헌_S_150367.java
+++ 20240717/S_150367/강지헌_S_150367.java
@@ -7,16 +7,12 @@ class Solution {
     
     private int chk(long n) {
         String s = Long.toBinaryString(n);
-        int t = (int) Math.pow(2, Integer.toBinaryString(s.length()).length()) - 1;
+        int t = (int)Math.pow(2,Integer.toBinaryString(s.length()).length()) - 1;
         String fill = String.format("%"+t+"s",s).replace(' ','0');
         for (int i = 1; i <= fill.length(); i++) {
-            if (i % 2 == 0 && fill.charAt(i - 1) == '0') {
-                int j = Integer.toBinaryString(i & -i).length() - 1;
-                int st = i & ~(1 << j);
-                int en = (1 << (j + 1)) - 1 + st;
-                for (j = st; j < en; j++) {
-                    if (fill.charAt(j) == '1') return 0;
-                }
+            if (i%2 == 0 && fill.charAt(i-1) == '0') {
+                int h = (i & (-i)) / 2;
+                if (fill.charAt(i-h-1)-'0' + fill.charAt(i+h-1)-'0' != 0) return 0;
             }
         }
         return 1;

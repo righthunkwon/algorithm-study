bf3872e35dce5910c5bf82f8e10e23a0f0591c23
Fezz <wlgjs0458@naver.com>
2024-08-06 11:04:31 +0900

    Update 강지헌_S_17687.java

--- 20240806/S_17687/강지헌_S_17687.java
+++ 20240806/S_17687/강지헌_S_17687.java
@@ -2,19 +2,19 @@ import java.util.*;
 class Solution {
     public String solution(int n, int t, int m, int p) {
         String answer = "";
-		List<Character> arr = new ArrayList<Character>();
-		arr.add('0');
+	List<Character> arr = new ArrayList<Character>();
+	arr.add('0');
         for(int i=1;arr.size()<t*m;i++) {
-            int j=i;
-            String s = "";
-			while (j!=0) {
-				int mod=j%n;
-				s =(char)(mod>=10?'A'+mod-10:'0'+mod)+s;
-				j = j / n;
-			}
-			for (char c : s.toCharArray()) arr.add(c);
+            	int j=i;
+            	String s = "";
+		while (j!=0) {
+			int mod=j%n;
+			s =(char)(mod>=10?'A'+mod-10:'0'+mod)+s;
+			j = j / n;
+		}
+		for (char c : s.toCharArray()) arr.add(c);
         }
-		for (int i = p-1; answer.length() < t; i += m) answer += arr.get(i);
-		return answer;  
+	for (int i = p-1; answer.length() < t; i += m) answer += arr.get(i);
+	return answer;  
     }
 }

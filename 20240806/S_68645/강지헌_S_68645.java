f50dc3892565d14bed10ebb26b80eaef9e9bf44c
Fezz <wlgjs0458@naver.com>
2024-08-06 14:16:55 +0900

    Update 강지헌_S_68645.java

--- 20240806/S_68645/강지헌_S_68645.java
+++ 20240806/S_68645/강지헌_S_68645.java
@@ -4,7 +4,7 @@ class Solution {
         int[][] map = new int[n][n];
         int[] answer = new int[n*(n-1)/2+n];
         int dir=0, x=0, y=-1;
-        for(int i = 1; i <= n*(n-1)/2+n; i++) {
+        for(int i=1; i<=n*(n-1)/2+n; i++) {
             if(dir==0) {
                 y++;
                 if((y+1>=n || map[y+1][x]!=0)) dir=(dir+1)%3;
@@ -19,8 +19,8 @@ class Solution {
             }
             map[y][x] = i;
         }
-        for(int i = 0,t=0; i < map.length; i++) {
-            for (int j = 0; j < i + 1; j++,t++) answer[t] = map[i][j];
+        for(int i=0,t=0; i<map.length; i++) {
+            for (int j=0; j<i+1; j++,t++) answer[t] = map[i][j];
         }
         return answer;
     }

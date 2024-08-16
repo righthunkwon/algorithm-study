ab38fd2609169e02ae1a82364b3ce7aae0a6fc13
LEEKH109 <dlrjsgml109@gmail.com>
2024-08-16 23:35:41 +0900

    [건희][8월 3주차][시소 짝꿍][리팩토링]

--- 20240820/S_152996/이건희_S_152996.java
+++ 20240820/S_152996/이건희_S_152996.java
@@ -1,5 +1,22 @@
 class Solution {
     public long solution(int[] weights) {
+        long result = 0;
+        long[] torque = new long[4001];
+        for (int weight : weights) {
+            result += torque[weight];// 1:1
+            result += torque[weight * 2];// 4:2
+            if (weight % 2 == 0) result += torque[weight / 2];// 2: 4
+            if (weight % 3 == 0) result += torque[weight * 2 / 3];// 2:3
+            if (weight % 2 == 0) result += torque[weight * 3 / 2];// 3:2
+            if (weight % 4 == 0) result += torque[weight * 3 / 4];// 3:4
+            if (weight % 3 == 0) result += torque[weight * 4 / 3];// 4:3
+            torque[weight]++;
+        }
+        return result;
+    }
+}
+class Solution {
+    public long solution(int[] weights) {
         long answer = 0;
         long[] count = new long[1001];
         long[] torque = new long[4001];

a1b8704195789852f96e03aaaf157b01ac662573
Kim SunHyeok <132821972+sunhyeok99@users.noreply.github.com>
2024-08-20 15:07:30 +0900

    Create 김선혁_S_42893_미구현.java

---
+++ 20240820/S_42893/김선혁_S_42893_미구현.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String word, String[] pages) {
+        int answer = 0;
+        // 기본점수는 hi등장할 때마다 up
+        // 외부 링크수도 각각 등장할때마다 1, 2,3씩 업
+        
+        // 입력에서 각자 점수를 구해서
+        // 그냥 매칭점수가 가장 높은 웹페이지 구하면됨
+        // 그냥 구현인데 입력을 어떻게 받는지 모르겠음..
+        
+        
+        
+        return answer;
+    }
+}

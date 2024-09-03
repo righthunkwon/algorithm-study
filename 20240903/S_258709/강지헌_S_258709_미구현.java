a1cc5d5e72421abfaf1c3bc1997f89a811635457
Fezz <wlgjs0458@naver.com>
2024-09-03 16:50:40 +0900

    Create 강지헌_S_258709_미구현.java

---
+++ 20240903/S_258709/강지헌_S_258709_미구현.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int[][] dice) {
+        int[] answer = {};
+        // 비트마스킹을 통해 어떤 주사위가 A를 선택할지 정하고
+        // 승률을 기록하며 최대 승률이 나올때 갱신해줌
+        return answer;
+    }
+}

8284ae6ca7bc8856304e0ae5b68c2ebf12ac6e1a
hajaeryul <113097210+hajaeryul@users.noreply.github.com>
2024-09-24 18:34:05 +0900

    Update 하재률_S_340211.js

--- 20240924/S_340211/하재률_S_340211.js
+++ 20240924/S_340211/하재률_S_340211.js
@@ -1,31 +1,57 @@
-function solution(diffs, times, limit) {
-    // 이분탐색 문제
-    const isPossible = (level) => {
-        // diffs[0]은 1 고정이니까 무조건 푼다
-        let time = times[0];
-        for(let i = 1; i < diffs.length; i++) {
-            // 레벨이 난이도에 못미치면 전에거 풀고
-            if(level < diffs[i]) time += (diffs[i] - level) * (times[i-1] + times[i]);
-            // 이번문제 풀기
-            time += times[i];
-        }
-        // 다 풀 수 있으면 true 뱉기
-        return time <= limit;
+function solution(points, routes) {
+    var answer = 0;
+    // 최단 거리
+    // 세로 -> 가로
+    // 경로 저장해서 2개이상 같으면 충돌+
+    
+    const memo = [];
+    
+    // 현재 위치, 타겟 위치 넣으면 다음 좌표 뱉어라
+    const getPosition = (r, c, targetR, targetC) => {
+        if(r !== targetR) return r < targetR ? [r+1, c] : [r-1, c];
+        if(c !== targetC) return c < targetC ? [r, c+1] : [r, c-1];
+        return [r, c];
     }
     
-    // diffs[0]은 1
-    let l = 1;
-    let r = limit;
-    
-    // 이분탐색
-    while(l < r) {
-        let mid = Math.floor((l + r) / 2);
+    // 로봇 경로 순회
+    routes.forEach((route, robotIdx) => {
+        // 출발점
+        let [curR, curC] = points[route[0] - 1];
         
-        // 풀 수 있으면 오른쪽 당기기
-        if(isPossible(mid)) r = mid;
-        // 못 풀면 왼쪽 당기기
-        else l = mid + 1;
+        // 로봇 경로 저장용
+        memo[robotIdx] = [[curR, curC]];
+        
+        route.slice(1).forEach(targetIdx => {
+            const [targetR, targetC] = points[targetIdx - 1];
+            
+            while(curR !== targetR || curC !== targetC) {
+                [curR, curC] = getPosition(curR, curC, targetR, targetC);
+                memo[robotIdx].push([curR, curC]);
+            }
+        })
+    })
+    // console.log(memo)
+    
+    const max = Math.max(...memo.map(p => p.length));
+    for(let i = 0; i < max; i++) {
+        const map = new Map();
+        
+        for(let j = 0; j < routes.length; j++) {
+            // 해당 시간대에 로봇 경로가 존재하면
+            if(memo[j][i]) {
+                const [r, c] = memo[j][i];
+                const key = `${r}, ${c}`;
+                
+                map.set(key, (map.get(key) || 0) + 1);
+            } 
+        }
+        
+        // console.log(i+'초', map)
+        map.forEach(cnt => {
+            if(cnt > 1) answer++;
+        })
     }
     
-    return l;
+    
+    return answer;
 }

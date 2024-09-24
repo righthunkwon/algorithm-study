be3a944d19a3d634f9556f971108bd23a915138d
Kim SunHyeok <132821972+sunhyeok99@users.noreply.github.com>
2024-09-24 15:00:31 +0900

    Create 김선혁_S_340210_미구현.java

---
+++ 20240924/S_340210/김선혁_S_340210_미구현.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String[] solution(String[] expressions) {
+        String[] answer = {};
+        // 2~9진법 중 하나를 찾고 
+        // X가 포함된 수식 계산
+        
+        // 우선 수식에서 ,를 기준으로 다 배열에 넣고
+        // X가 포함된 식을 제외하고
+        // 2~9진법 모두 dfs로 돌아서 확인
+        // 만약 2개 이상일 경우 ?로 출력
+        for(int i = 0 ;i<expressions.length;i++){
+            
+            
+            
+        }
+        
+        return answer;
+    }
+}

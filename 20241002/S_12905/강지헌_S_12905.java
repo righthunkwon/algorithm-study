2c85c3e408b5b1074d0ab51443c321f436a06b23
Fezz <wlgjs0458@naver.com>
2024-10-01 12:57:20 +0900

    Update 강지헌_S_12905.java

--- 20241002/S_12905/강지헌_S_12905.java
+++ 20241002/S_12905/강지헌_S_12905.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(int [][]board) {
-		int n=0;
-		for(int i=0;i<board.length;i++) {
-			for(int j=0;j<board[0].length;j++) {
-				if(board[i][j]==1) {
-					if(i != 0 && j != 0) board[i][j] = Math.min(board[i-1][j-1], Math.min(board[i-1][j], board[i][j-1])) + 1;
-					n=n<board[i][j]?board[i][j]:n;
-				}
+	int n=0;
+	for(int i=0;i<board.length;i++) {
+		for(int j=0;j<board[0].length;j++) {
+			if(board[i][j]==1) {
+				if(i != 0 && j != 0) board[i][j] = Math.min(board[i-1][j-1], Math.min(board[i-1][j], board[i][j-1])) + 1;
+				n=n<board[i][j]?board[i][j]:n;
 			}
 		}
-		return n*n;
+	}
+	return n*n;
     }
 }

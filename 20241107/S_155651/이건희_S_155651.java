388653d04d7ea836b1bde326369201ecef3217a6
LEEKH109 <dlrjsgml109@gmail.com>
2024-11-04 16:05:19 +0900

    [건희][11월 1주차] 호텔 대실 리팩

--- 20241107/S_155651/이건희_S_155651.java
+++ 20241107/S_155651/이건희_S_155651.java
@@ -1,3 +1,4 @@
+// 기본 풀이
 import java.util.*;
 class Solution {
     public int solution(String[][] book_time) {
@@ -27,5 +28,30 @@ class Solution {
             }
         }
         return list.size();
+    }
+}
+// 우선순위 큐 풀이
+import java.util.*;
+class Solution {
+    public int solution(String[][] book_time) {
+        int[][] book_time_int = new int[book_time.length][2];
+        for(int i = 0; i < book_time.length; i++){
+            int s = Integer.parseInt(book_time[i][0].substring(0,2))*60 
+                        + Integer.parseInt(book_time[i][0].substring(3,5));
+            int e = Integer.parseInt(book_time[i][1].substring(0,2))*60 
+                        + Integer.parseInt(book_time[i][1].substring(3,5));
+            book_time_int[i][0] = s; book_time_int[i][1] = e; 
+        }
+        Arrays.sort(book_time_int, (a,b) -> a[0]-b[0]);
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for(int i = 0; i < book_time_int.length; i++){
+            int s = book_time_int[i][0];
+            int e = book_time_int[i][1] + 10; 
+            if(!q.isEmpty() && q.peek()<=s){
+                q.poll();
+            }
+            q.offer(e);
+        }
+        return q.size();
     }
 }

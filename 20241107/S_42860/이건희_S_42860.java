9d815eebe91a0af69d5d6a2f9289e6f8795844d4
LEEKH109 <dlrjsgml109@gmail.com>
2024-11-05 16:55:09 +0900

    [건희][11월 1주차] 조이스틱_그리디

--- 20241107/S_42860/이건희_S_42860.java
+++ 20241107/S_42860/이건희_S_42860.java
@@ -1,3 +1,35 @@
+// 그리디 풀이 O(N)으로 최적화
+class Solution {
+    // "BBABAAABAABB" => 11
+    // "BBBBAAAABA" => 12
+    public int solution(String name) {
+        int n = name.length();
+        int cnt = 0;
+        int minMove = n - 1;
+
+        for (int i = 0; i < n; i++) {
+            char tmp = name.charAt(i);
+            cnt += swap(tmp);
+
+            int idx = i + 1;
+            while (idx < n && name.charAt(idx) == 'A') {
+                idx++;
+            }
+            int moveRL = i + i + n - idx;
+            int moveLR = (n - idx) + (n - idx) + i;
+            int move = Math.min(moveRL, moveLR);
+            minMove = Math.min(minMove, move);
+        }
+
+        cnt += minMove;
+        return cnt;
+    }
+
+    public int swap(char target) {
+        return Math.min(target - 'A', 'Z' - target + 1);
+    }
+}
+
 // 재귀로 풀기 => 그리디로 다시 풀어보자
 import java.util.*;
 class Solution {

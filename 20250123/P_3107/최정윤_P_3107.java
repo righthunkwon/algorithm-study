35ffd5c470b57116c8010b6cc9a6a79d75cce023
jungyounn <141210401+jungyounn@users.noreply.github.com>
2025-01-23 14:40:03 +0900

    최정윤_P_3107.java 업데이트

--- 20250123/P_3107/최정윤_P_3107.java
+++ 20250123/P_3107/최정윤_P_3107.java
@@ -6,14 +6,12 @@ public class Main{
         Scanner sc= new Scanner(System.in);
         StringBuilder answer=new StringBuilder();
         String[] input= (sc.next()+" ").split(":");
+				//ㅁㅏ지막에 1::이면 split결과가 "1"만 나옴 "1","",""이렇게 나오는 줄 알았는데 ... 
+				//그거 처리하려고 붙였다가 제거하는 작업 .. 
         input[input.length-1]=input[input.length-1].substring(0,input[input.length-1].length()-1);
-        int cnt=7;
+        int cnt=7;//:의 개수
         for(int i=0;i<input.length;i++){
-            if(input[i].length()==0) {
-  
-    
-            		
-            	
+            if(input[i].length()==0) { 
                 for(int j=1;j<=8-input.length+1;j++){
                     answer.append("0000");
                     if(cnt>0){
@@ -21,14 +19,14 @@ public class Main{
                         cnt--;
                     }
                 }
-                              if(input[i+1].length()==0){
+                if(input[i+1].length()==0){
                     //    ㅁㅐㄴㅇㅏㅍ , ㅁㅐㄴ ㄷㅜㅣ
-                 answer.append("0000");
-                                  if(cnt>0){
-                                      answer.append(":");
-                                      cnt--;
-                                  }
-                                  i++;  
+                    answer.append("0000");
+                    if(cnt>0){
+                        answer.append(":");
+                         cnt--;
+                    }
+                     i++;  
                 }
             	
             }else if(input[i].length()==4){
